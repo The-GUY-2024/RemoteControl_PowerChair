498dd1e60f533bdc38a91df63407c5862fe40f00
Certified Humane <castrolwilliam@gmail.com>
2022-11-02 10:44:34 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,29 @@
+from asyncio import events
+import os
+import socket
+import sys
+from XInput import *
 
+
+class Client:
+    def __init__(self):
+        #Remote parameters 
+        set_deadzone(DEADZONE_TRIGGER, 10)
+        self.events = ""
+
+        self.address = ("192.168.0.7", 100)
+        self.s = ""
+        
+
+        while 1:
+            self.Remote_detection()
+
+    def Remote_detection(self):
+        self.events = get_events();
+        for event in self.events:
+            if (event.type == EVENT_CONNECTED):
+                print("Xbox Remote Connected ")
+            
+            elif (event.type == EVENT_DISCONNECTED):
+                print("Please connect the device")
+                exit()
